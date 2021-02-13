88134bf1a44c1d6ee2e56a5b0086f6b8f04b078e
AlbertCos <cosmenalberto@gmail.com>
2021-02-13 10:23:03 +0100

    Developing style for heading

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import {Paper, Typography, CssBaseline} from '@material-ui/core';
+import{makeStyles} from '@material-ui/core/styles';
+
+const useStyle = makeStyles((theme)=>({
+    root:{
+        width:'300px',
+        backgroundColor:'EBECF0',
+        marginLeft: theme.spacing(1),
+    },
+}));
+
 export default function List () {
+    const classes = useStyle ();
     return (
         <div>
-            <Paper>
+            <Paper ClassName={classes.root}>
                 <CssBaseline/>
                 <Typography>
                     Todo
                 </Typography>
-
             </Paper>
         </div>
-    )
+    );
 }
