7abd1eef1930d63bdb6a07310ed7f4a49ad75ce7
AlbertCos <cosmenalberto@gmail.com>
2021-02-13 10:11:42 +0100

    Instalation of Material-UI framework

---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Paper, Typography, CssBaseline} from '@material-ui/core';
+export default function List () {
+    return (
+        <div>
+            <Paper>
+                <CssBaseline/>
+                <Typography>
+                    Todo
+                </Typography>
+
+            </Paper>
+        </div>
+    )
+}
