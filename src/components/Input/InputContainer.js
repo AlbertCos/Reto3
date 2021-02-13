2cac61ad43681601f748aacb19f142c260f46e39
AlbertCos <cosmenalberto@gmail.com>
2021-02-13 12:18:45 +0100

    Creating inputt component

--- src/components/Input/InputContainer.js
+++ src/components/Input/InputContainer.js
@@ -1,12 +1,25 @@
 import { Paper, Typography } from '@material-ui/core'
 import React from 'react'
+import{makeStyles} from '@material-ui/core/styles';
+
+const useStyle = makeStyles((theme)=>({
+    root:{
+        marginTop:theme.spacing(2),
+    },
+    addCard:{
+        padding:theme.spacing(1,1,1,2),
+        margin: theme.spacing(1),
+    },
+}));
 
 export default function InputContainer() {
+    const classes = useStyle ();
+
     return (
-        <div>
-            <Paper>
+        <div className={classes.root}>
+            <Paper className={classes.addCard}>
                 <Typography>
-                    Add a new Card
+                   + Add a new Card
                 </Typography>
             </Paper>
         </div>
