178a1bafc92ded82f35ca0c3586c421ae7cf951d
AlbertCos <cosmenalberto@gmail.com>
2021-02-13 12:24:02 +0100

    Creating input Card componentt

--- src/components/Input/InputContainer.js
+++ src/components/Input/InputContainer.js
@@ -1,6 +1,6 @@
 import { Paper, Typography } from '@material-ui/core'
 import React from 'react'
-import{makeStyles} from '@material-ui/core/styles';
+import{fade, makeStyles} from '@material-ui/core/styles';
 
 const useStyle = makeStyles((theme)=>({
     root:{
@@ -9,6 +9,10 @@ const useStyle = makeStyles((theme)=>({
     addCard:{
         padding:theme.spacing(1,1,1,2),
         margin: theme.spacing(1),
+        background:"#EBECF0",
+        "&:HOVER":{
+            backgroundColor:fade("#000", 0.25),
+        },
     },
 }));
 
